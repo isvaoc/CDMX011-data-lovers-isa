d12cb53d94fe012fd7f70ed6aa7c00da67d1fe6c
Isabel <isvaoc@gmail.com>
2021-07-14 19:22:38 -0500

    dandole estilo y creando cajitas

--- src/main.js
+++ src/main.js
@@ -6,9 +6,31 @@ import data from './data/pokemon/pokemon.js';
 /*console.log(example, data);*/
 function visualize(data){
     for (let prop in data["pokemon"]){
-        console.log(data["pokemon"][prop]["num"])
-        console.log(data["pokemon"][prop]["name"])
-        console.log(data["pokemon"][prop]["img"])
+        let numero = data["pokemon"][prop]["num"]
+        let nombre = data["pokemon"][prop]["name"]
+        let link = data["pokemon"][prop]["img"]
+        let div = document.createElement("div")
+        /*div.id = "div-poke-" + prop
+        div.class = "divs-poke"*/
+        div.setAttribute("class", "divs-poke")
+        div.setAttribute("id", "div-poke-" + prop)
+        let imgn = document.createElement("img")
+        imgn.src=link
+        imgn.style.display="inline-block"
+        let divroot = document.getElementById("root")
+        divroot.appendChild(div)
+        div.appendChild(imgn)
+        let div2 = document.createElement("div")
+        div2.style.display="inline-block"
+        let pNum = document.createElement("p")
+        pNum.innerText = numero 
+        
+        let pName = document.createElement("p")
+        pName.innerText = nombre 
+        
+        div2.appendChild(pNum)
+        div2.appendChild(pName)
+        div.appendChild(div2)
     }
 }
 visualize(data)
