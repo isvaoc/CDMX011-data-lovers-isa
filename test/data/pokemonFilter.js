25e7e8bdb351b1d76d440176049b3c7d1eb98700
Daniela Piña <danielapina@MacBook-Pro-de-Daniela.local>
2021-07-28 17:59:47 -0500

    test al 70 y css fondo

--- test/data/pokemonFilter.js
+++ test/data/pokemonFilter.js
@@ -1,22 +1,22 @@
 export default [
-    {
-      "num": "001",
-      "name": "bulbasaur",
-      "generation": {
-        "num": "generation i",
-        "name": "kanto",
-      },
-      "pokemon-rarity": "normal",
-      "type": ["grass", "poison"],
+  {
+    num: "251",
+    name: "celebi",
+    generation: {
+      num: "generation ii",
+      name: "johto",
     },
-    {
-        "num": "251",
-        "name": "celebi",
-        "generation": {
-          "num": "generation ii",
-          "name": "johto",
-        },
-        "pokemon-rarity": "mythic",
-        "type": ["psychic", "grass"],
-      },
-    ]
+    "pokemon-rarity": "mythic",
+    type: ["psychic", "grass"],
+  },
+  {
+    num: "001",
+    name: "bulbasaur",
+    generation: {
+      num: "generation i",
+      name: "kanto",
+    },
+    "pokemon-rarity": "normal",
+    type: ["grass", "poison"],
+  },
+];
